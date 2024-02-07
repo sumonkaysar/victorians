178c03842a87f61f906631e528c2bb239fe3ce99
Rahad23 <rahadhasan33675@gmail.com>
2024-02-07 22:43:09 +0600

    working message controller

--- controllers/message_controller.js
+++ controllers/message_controller.js
@@ -1,4 +1,4 @@
-const { userMessage } = require("../mongoDBConfig/collections");
+const { userMessage, usersCollection } = require("../mongoDBConfig/collections");
 const { createDoc, readDoc } = require("../utils/mongoQueries");
 const { ObjectId } = require("mongodb");
 const { uploadFile } = require("../utils/uploadFile");
@@ -50,6 +50,72 @@ const getMessage = async (req, res) => {
   }
 };
 
+
+const readUnreadMessage = async (req, res) => {
+  try {
+    // Extract user ID from request params
+    const userId = req.params.userId;
+    const reqBodyData = req.body;
+    // Find the user by ID
+    const findUser = await usersCollection().findOne({ _id: new ObjectId(userId) });
+    // Check if the user exists
+    if (!findUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update the user's field
+    findUser.readUnreadMessage = {...reqBodyData, messageUserId: findUser?._id}; // Add the readUnread field with the specified value
+
+    // Update the user in the database
+    await usersCollection().updateOne(
+      { _id: new ObjectId(userId) },
+      { $set: { readUnreadMessage: findUser.readUnreadMessage} }
+    );
+
+    // Send a success response
+    res.json({ message: "User field updated successfully", readUnreadMessage: findUser });
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+const sortingUserWithMessage= async(req, res)=>{
+  const allUsers = await readDoc(usersCollection);
+  const allMessage = await readDoc(userMessage);
+
+  const extractedData = allMessage.map(message => ({
+    receiver: message.receiver,
+    authorId: message.authorId,
+    timestamp: message.timestamp
+  }));
+
+// Remove 'new ObjectId'
+const userIds = new Set(extractedData.flatMap(data => [data.receiver, data.authorId]));
+
+
+const usersWithMatchingIds = allUsers.filter(user => userIds.has(user._id.toString()));
+
+
+usersWithMatchingIds.sort((a, b) => {
+  const timestampA = Math.max(
+    ...extractedData
+      .filter(data => data.receiver === a._id.toString() || data.authorId === a._id.toString())
+      .map(data => data.timestamp)
+  );
+  const timestampB = Math.max(
+    ...extractedData
+      .filter(data => data.receiver === b._id.toString() || data.authorId === b._id.toString())
+      .map(data => data.timestamp)
+  );
+  return timestampB - timestampA;
+});
+
+  res.send(usersWithMatchingIds)
+
+}
+
 const getLatestUsers = async (req, res) => {
   try {
     const users = await userMessage()
@@ -75,4 +141,6 @@ module.exports = {
   saveMessage,
   getMessage,
   getLatestUsers,
+  readUnreadMessage,
+  sortingUserWithMessage
 };
