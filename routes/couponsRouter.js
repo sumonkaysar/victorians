4b2369f8d243567d7663f7fdc5d158e1245ff418
sumonkaysar <msk.sumon.kaysar@gmail.com>
2024-02-09 20:24:14 +0600

    -

--- routes/couponsRouter.js
+++ routes/couponsRouter.js
@@ -1,4 +1,4 @@
-const { getAllCoupons, createCoupon, updateCoupon, deleteCoupon, matchCoupon, findCoupon } = require("../controllers/couponsController")
+const { getAllCoupons, createCoupon, updateCoupon, deleteCoupon, matchCoupon, findCoupon, checkCoupon } = require("../controllers/couponsController")
 const { verifyAdmin } = require("../middlewares/verifyJWT")
 
 const couponsRouter = require("express").Router()
@@ -14,5 +14,7 @@ couponsRouter.delete("/:id", verifyAdmin, deleteCoupon)
 couponsRouter.post("/match", verifyAdmin, matchCoupon)
 
 couponsRouter.post("/check-validity", findCoupon)
+
+couponsRouter.get("/products/:productId", checkCoupon)
 
 module.exports = couponsRouter
